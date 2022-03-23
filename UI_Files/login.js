057ea19e8abfe7e4e171f32ff0a7ba083c419009
Marcus Glover <marcus961@revature.net>
2022-03-23 16:08:44 -0400

    Added JS file for login html page

---
+++ UI_Files/login.js
@@ -0,0 +1,16 @@
+const url = ''; // Url of tomcat server
+
+let loginBtn = document.getElementById('loginBtn');
+loginBtn.addEventListener('click', loginFunction);
+
+
+function loginFunction()
+{
+    let u_name = document.getElementById('u_name').textContent;
+    let p_name = document.getElementById('p_name').textContent;
+
+    // Put data into object
+    // Convert to JSON
+
+    // Use Fetch API to send/recieve info?
+}
