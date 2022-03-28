39555b44905c176e5893b55c2f1aaa19faa09970
Marcus Glover <marcus961@revature.net>
2022-03-27 21:55:51 -0400

    Implemented sign up functionality, user is persisted into database

--- UI_Files/users/login.js
+++ UI_Files/users/login.js
@@ -1,12 +1,12 @@
-const testBaseURL = 'http://127.0.0.1:5500/UI_Files/';          // ONLY FOR TESTING
-const indexPage = 'index.html';
+//const testBaseURL = 'http://127.0.0.1:5500/UI_Files/';          // ONLY FOR TESTING
+const indexPage = 'http://127.0.0.1:5500/UI_Files/index.html';
 
 const thisURL = window.location.href;
 const adminURL = 'admins/adminPage.html';       // Page for admins
 
 // Login Button
 let loginBtn = document.getElementById('loginBtn');
-loginBtn.addEventListener('button', loginFunction);
+loginBtn.addEventListener('click', loginFunction);
 
 // Back Button
 let backBtn = document.getElementById('backBtn');
@@ -16,9 +16,9 @@ backBtn.addEventListener('click', goBack);
 async function loginFunction()
 {
     // Get username from DOM element
-    let username = document.getElementById('username').textContent;
+    let username = document.getElementById('username').value;
     // Get password from DOM element
-    let password = document.getElementById('password').textContent;
+    let password = document.getElementById('password').value;
 
     // Wrap into a user object
     let userObj = 
@@ -27,21 +27,28 @@ async function loginFunction()
         password: password
     }
 
-    // POST User object to validate credentials
-    let response = await fetch(thisURL, 
-        {
-            method:'POST',  // POST HTTP method
-            headers:{"Content-type":"application/json"},    // Indicate JSON object
-            body: JSON.stringify(userObj)   // Convert to JSON to send
-        });
-
-    // Based on response, navigate user to correct HTML page
-    if (response) window.location.href = adminURL;
+    if (userObj.username && userObj.password)
+    {
+        // POST User object to validate credentials
+        let response = await fetch(thisURL, 
+            {
+                method:'POST',  // POST HTTP method
+                headers:{"Content-type":"application/json"},    // Indicate JSON object
+                body: JSON.stringify(userObj)   // Convert to JSON to send
+            });
+    
+        // Based on response, navigate user to correct HTML page
+        if (response.status == 204) window.location.href = adminURL;
+    }
+    else
+    {
+        alert("Missing a field");
+    }
 }
 
 // Function when Back button is clicked
 function goBack()
 {
-    window.location.href = testBaseURL + indexPage;
+    window.location.href = indexPage;
     // history.back();     // Goes back a page
 }
--- UI_Files/users/signUp.js
+++ UI_Files/users/signUp.js
@@ -1,12 +1,14 @@
-const testBaseURL = 'http://127.0.0.1:5500/UI_Files/';          // ONLY FOR TESTING
+const baseURL = 'http://127.0.0.1:5500/UI_Files/';          // ONLY FOR TESTING
 const indexPage = 'index.html';
-
 
 const thisURL = window.location.href;
 const employeeURL = 'employees/employeePage.html';    // Page for employees
 const managerURL = 'managers/managerPage.html';      // Page for managers
 const adminURL = 'admins/adminPage.html';          // Page for admins
 
+
+const fetchURL = 'http://localhost:8080/';  // <-- URL to use when accessing API
+const servletURL = 'users';                // <-- Servlet whose methods should be used
 
 // Constants for the dropdown selection
 const EMP = "0";
@@ -15,7 +17,7 @@ const ADMIN = "2";
 
 // Sign Up Button
 let signUpBtn = document.getElementById('signUpBtn');
-signUpBtn.addEventListener('button', signUpFunction);
+signUpBtn.addEventListener('click', signUpFunction);
 
 // Back Button
 let backBtn = document.getElementById('backBtn');
@@ -34,31 +36,48 @@ async function signUpFunction()
         role_ID: document.getElementById('role').value      // Drop down value
     }
 
-    // POST User object to create user in backend
-    let response = await fetch(thisURL, 
-    {
-        method:'POST',  // POST HTTP method
-        headers:{"Content-type":"application/json"},    // Indicate JSON object
-        body: JSON.stringify(userObj)   // Convert to JSON to send
-    });
-
-
-    // User was created successfully
-    if (response.status == 204) 
-    {
-        // Switch page
-        switch(userObj.role_ID)
+    // Check properties of user object, ensure it has values
+    let isValid = Object.values(userObj).every(value => 
         {
-            case EMP:       // Employees
-                window.location.href = employeeURL;
-                break;
-            case MANAGER:   // Managers
-                window.location.href = managerURL;
-                break;
-            case ADMIN:     // Admins
-                window.location.href = adminURL;
-                break;
+            if (!value)
+                return false;
+            else
+                return true;
+        })
+
+    // User object is a valid object
+    if (isValid)
+    {
+        // POST User object to create user in backend
+        let response = await fetch(`${fetchURL + servletURL}`,  // <-- URL/servlet to fetch
+        {
+            method:'POST',  // POST HTTP method
+            headers:{"Content-type":"application/json"},    // Indicate JSON object
+            body: JSON.stringify(userObj)   // Convert to JSON to send
+        });
+    
+    
+        // User was created successfully
+        if (response.status == 204) 
+        {
+            // Switch page
+            switch(userObj.role_ID)
+            {
+                case EMP:       // Employees
+                    window.location.href = employeeURL;
+                    break;
+                case MANAGER:   // Managers
+                    window.location.href = managerURL;
+                    break;
+                case ADMIN:     // Admins
+                    window.location.href = adminURL;
+                    break;
+            }
         }
+    }
+    else
+    {
+        alert("Missing a field");
     }
 }
 
@@ -66,6 +85,6 @@ async function signUpFunction()
 // Function when Back button is clicked
 function goBack()
 {
-    window.location.href = testBaseURL + indexPage;
+    window.location.href = baseURL + indexPage;
     // history.back();     // Goes back a page
 }
