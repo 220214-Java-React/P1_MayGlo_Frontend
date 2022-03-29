00b607bb08b8af266972441c0f21d09bc1834825
Marcus Glover <marcus961@revature.net>
2022-03-29 17:52:48 -0400

    Added dynamic population of credentials on deletion page

--- UI_Files/users/admins/deleteUser.js
+++ UI_Files/users/admins/deleteUser.js
@@ -1,6 +1,10 @@
 const thisURL = window.location.href;
 const adminURL = 'adminPage.html';
 
+const fetchURL = 'http://localhost:8080/';  // <-- URL to use when accessing API
+const servletURL = 'users/';                // <-- Servlet whose methods should be used
+
+
 // Constants for the dropdown selection
 const EMP = "EMPLOYEE";
 const MANAGER = "MANAGER";
@@ -20,6 +24,18 @@ deleteBtn.addEventListener('click', deleteUser);
 let cancelBtn = document.getElementById('cancelBtn');
 cancelBtn.addEventListener('click', cancelCreate);
 
+// Credentials to show for the user found
+let userFound =
+{
+    // Get each element on HTML page
+    u_name : document.getElementById("username"),
+    p_word : document.getElementById("password"),
+    email : document.getElementById("email"),
+    given_name : document.getElementById("given_name"),
+    surname : document.getElementById("surname"),
+    role_ID : document.getElementById("role")
+};
+
 async function searchUser()
 {
     // Username to search for
@@ -29,15 +45,35 @@ async function searchUser()
     if (searchName)
     {
         // Search in backend
-        await fetch(thisURL, 
-            {
-                method:'POST',  // POST HTTP method
-                headers:{"Content-type":"application/json"},    // Indicate JSON object
-                body: JSON.stringify(searchName)
-            })
-    
-        // If a user is found, un-hide hidden form
-        document.getElementById('credentials').removeAttribute("hidden");
+        let data = await fetch(`${fetchURL + servletURL + '?username=' + searchName}`,
+        {
+            method:'GET',  // POST HTTP method
+            headers:{"Content-type":"application/json"},    // Indicate JSON object
+        })
+        .then(response => response.json());
+
+        console.log(data);
+
+        // Data needed:
+        // username
+        // password
+        // email
+        // given_name
+        // surname
+        // role_ID
+        // Set value for each HTML element
+        if (data)
+        {
+            // If a user is found, un-hide hidden form
+            document.getElementById('credentials').removeAttribute("hidden");
+            
+            userFound.u_name.value = data.username;
+            userFound.p_word.value = data.password;
+            userFound.email.value = data.email;
+            userFound.given_name.value = data.given_name;
+            userFound.surname.value = data.surname;
+            userFound.role_ID.value = data.role_ID;
+        }
     }
 }
 
@@ -45,6 +81,7 @@ async function searchUser()
 function deleteUser()
 {
     console.log("Button Works");
+    
 }
 
 
