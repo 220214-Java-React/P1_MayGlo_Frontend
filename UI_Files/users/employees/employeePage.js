4b7358e17ddee362e979150710345790a01322a7
Asheton S. M <AshetonSM@gmail.com>
2022-03-30 16:25:17 -0500

    Added sort by ID and Amount to the employee page. Also added a dollar sign to amount rows

--- UI_Files/users/employees/employeePage.js
+++ UI_Files/users/employees/employeePage.js
@@ -68,7 +68,7 @@ function createRow(reimbursementItem)
     // Amount column
     let reimbAmt = document.createElement("td");
     reimbAmt.className = "reimbCell";
-    reimbAmt.innerText = amount;
+    reimbAmt.innerText = "$" + amount;
 
     // Time Submission column
     let reimbSubmit = document.createElement("td");
@@ -116,9 +116,9 @@ function filterType() {
             }
         }
     }
-  }
+}
 
-  function filterStatus() {
+function filterStatus() {
     // Declare variables
     var filter, table, tr, td, i, txtValue;
     filter = statusDrop.value;
@@ -137,8 +137,65 @@ function filterType() {
             }
         }
     }
-  }
+}
 
+// Nearly unmodified code from w3schools' example of table sorting
+function sortByRowInteger(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("reimbTable");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+      // Start by saying: no switching is done:
+      switching = false;
+      rows = table.rows;
+      /* Loop through all table rows (except the
+      first, which contains table headers): */
+      for (i = 0; i < (rows.length - 1); i++) {
+        // Start by saying there should be no switching:
+        shouldSwitch = false;
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+        x = rows[i].getElementsByTagName("TD")[n];
+        y = rows[i + 1].getElementsByTagName("TD")[n];
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+        if (dir == "asc") {
+          if (parseInt(x.innerHTML) > parseInt(y.innerHTML)) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (parseInt(x.innerHTML) < parseInt(y.innerHTML)) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount ++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+        if (switchcount == 0 && dir == "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+
+
+}
 
 // Converts Type ID
 function convertType(ID)
